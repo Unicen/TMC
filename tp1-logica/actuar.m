469487ec0fa50c57d280cc947aee1a23f10a3566
José Ignacio Orlando <ignaciorlando@gmail.com>
2017-03-21 19:26:58 -0300

    Acomodé un bug

--- tp1-logica/actuar.m
+++ tp1-logica/actuar.m
@@ -3,10 +3,8 @@ function actuar(objeto_observado)
 
   % TODO: Esta función la vamos a ir completando en clase
 
-  if proposicion1
-    % Completamos con una manera de actuar cuando 'proposicion1' evalúa en true
-  elseif proposicion2
-    % Completamos con una manera de actuar cuando 'proposicion2' evalúa en true
+  if (esPlaneta(objeto_observado) || esLuna(objeto_observado))
+    esquivar(objeto_observado);
   end
 
 end
--- tp1-logica/funciones-soporte/estaEnLaLista.m
+++ tp1-logica/funciones-soporte/estaEnLaLista.m
@@ -7,7 +7,7 @@ function esta = estaEnLaLista(nombre, lista)
   % busco si el valor en 'nombre' está en 'lista'
   for i = 1 : length(lista)
     % pregunto si el elemento actual de 'lista' es 'nombre'
-    if strcmp(lista{i}) == nombre
+    if strcmp(lista{i}, nombre)
       % si está asigno true a valor de verdad y no hace falta que siga ciclando
       valorDeVerdad = true;
       break
